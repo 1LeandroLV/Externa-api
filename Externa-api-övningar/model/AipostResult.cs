e11acc1e4e247995627da7984d899fce3fb4f76b
1LeandroLV <leandro.verdugo02@gmail.com>
2026-03-10 11:51:13 +0100

    fixade så rätt kod o rätt fil

---
+++ Externa-api-övningar/model/AipostResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Externa_api_övningar.model
+{
+    public class AipostResult
+    { 
+        public int Id { get; set; }
+        public string summary { get; set; }
+        public List<string> Tags { get; set; } = new();
+         public string Risk_Level { get; set; }
+    }
+}
--- Externa-api-övningar/services/OpenAiClient.cs
+++ Externa-api-övningar/services/OpenAiClient.cs
@@ -1,8 +1,20 @@
-﻿//bra för del 2
+﻿using System.Net.Http.Headers; //behövs för Authorization;
+using System.Text; //behövs för att bygga text
+using System.Text.Json; //behövs för att skapa och läsa json
+using Externa_api_övningar.Models; //behövs för att använda Post
+
+
+
+//bra för del 2
 // skicka text till open al + får json tillbacka
 namespace Externa_api_övningar.services
-{
-    public class OpenAiClient
+
+    public class OpenAiClient //prata med openAi
     {
-    }
-}
+      private readonly HttpClient _httpClient;
+        public OpenAiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+         }
+}  
+
