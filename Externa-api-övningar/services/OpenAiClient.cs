19c05a47f762271a9b07a914822e711c1317fbb2
1LeandroLV <leandro.verdugo02@gmail.com>
2026-03-10 13:07:33 +0100

    i openaiclient kan den nu  ta emot pots, 2 läsa api key,  förberda request

--- Externa-api-övningar/services/OpenAiClient.cs
+++ Externa-api-övningar/services/OpenAiClient.cs
@@ -7,14 +7,32 @@ using Externa_api_övningar.Models; //behövs för att använda Post
 
 //bra för del 2
 // skicka text till open al + får json tillbacka
-namespace Externa_api_övningar.services
+namespace Externa_api_övningar.services;
 
-    public class OpenAiClient //prata med openAi
+public class OpenAiClient //prata med openAi
+{
+    private readonly HttpClient _httpClient;
+    public OpenAiClient(HttpClient httpClient)
     {
-      private readonly HttpClient _httpClient;
-        public OpenAiClient(HttpClient httpClient)
+        _httpClient = httpClient;
+    }
+    //skapa metoden
+    public async Task<string> AnalyzePostAsync(List<Post> Post)
+    {
+        //hämta api nyckel från datorn 
+        var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        if (string.IsNullOrEmpty(apiKey)) //kollar om myckeln är tom 
         {
-            _httpClient = httpClient;
-         }
-}  
 
+            throw new Exception("OPENAI API saknas");
+        }
+        //detta skicka denna header till openal
+        _httpClient.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Bearer", apiKey);
+
+        // 1 ta emot posts
+        // 2läsa api key
+        // 3 förnerda request 
+    }
+
+}
